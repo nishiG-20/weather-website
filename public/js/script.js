61a8bd41d7e3d58ff34beab037e05c52e1a40f26
Nishant sharmaclear <nishantsharma783007@gmail.com>
2021-02-24 09:48:13 +0530

    Add two extra dot

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ let message2=document.querySelector('#message2')
 
 weatherForm.addEventListener('submit',(e)=>{
      e.preventDefault()
-     message1.textContent='Loading....'
+     message1.textContent='Loading......'
      const location=search.value
      fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
       response.json().then((data)=>{
